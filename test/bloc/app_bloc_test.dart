b73c3d681f99f4f1e18d504570355262bbfe47d5
p-jura <themadman@o2.pl>
2023-03-30 09:46:42 +0200

    last commit, with tests

--- test/bloc/app_bloc_test.dart
+++ test/bloc/app_bloc_test.dart
@@ -11,6 +11,8 @@ extension ToList on String {
 final text1Data = 'test1'.toUint8List();
 final text2Data = 'test2'.toUint8List();
 
+enum Errors { dummy }
+
 void main() {
   blocTest<ApplicationBloc, AppState>(
     'initial state of bloc should be empty',
@@ -21,7 +23,7 @@ void main() {
     ),
   );
   blocTest<ApplicationBloc, AppState>(
-    'load valid data',
+    'load valid data, should return state with data',
     build: () => ApplicationBloc(
       urls: [],
       urlPicker: (_) => '',
@@ -39,6 +41,68 @@ void main() {
       AppState(
         isLoading: false,
         data: text1Data,
+        error: null,
+      ),
+    ],
+  );
+
+  blocTest<ApplicationBloc, AppState>(
+    'should throw an error in url loader and catch it',
+    build: () => ApplicationBloc(
+      urls: [],
+      urlPicker: (_) => '',
+      urlLoader: (_) => Future.error(Errors.dummy),
+    ),
+    act: (bloc) => bloc.add(
+      LoadNextUrlEvent(),
+    ),
+    expect: () => [
+      AppState(
+        isLoading: true,
+        data: null,
+        error: null,
+      ),
+      AppState(
+        isLoading: false,
+        data: null,
+        error: Errors.dummy,
+      ),
+    ],
+  );
+  blocTest<ApplicationBloc, AppState>(
+    'load valid data, should return state with data twice',
+    build: () => ApplicationBloc(
+      urls: [],
+      urlPicker: (_) => '',
+      urlLoader: (_) => Future.value(text2Data),
+    ),
+    act: (bloc) {
+      bloc.add(
+        LoadNextUrlEvent(),
+      );
+      bloc.add(
+        LoadNextUrlEvent(),
+      );
+    },
+    expect: () => [
+      AppState(
+        isLoading: true,
+        data: null,
+        error: null,
+      ),
+      AppState(
+        isLoading: false,
+        data: text2Data,
+        error: null,
+      ),
+      AppState(
+        isLoading: true,
+        data: null,
+        error: null,
+      ),
+      AppState(
+        isLoading: false,
+        data: text2Data,
         error: null,
       ),
     ],
